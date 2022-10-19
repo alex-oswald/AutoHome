acaf39ed587e8ea0fa9b1334df672f081b440be0
Alex Oswald <alex.oswald@outlook.com>
2022-10-18 23:36:55 -0700

    Paging for Trigger History

--- src/AutoHome.Client/Components/ListQuery.cs
+++ src/AutoHome.Client/Components/ListQuery.cs
@@ -1,4 +1,6 @@
 ﻿using AutoHome.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace AutoHome.Client.Components;
 
@@ -12,4 +14,52 @@ public class ListQuery
         PageIndex = DefaultPagedRequest.DefaultPageIndex,
         PageSize = DefaultPagedRequest.DefaultPageSize,
     };
+}
+
+public static class NavigationManagerExtensions
+{
+    public static bool TryGetQueryString<T>(this NavigationManager manager, string key, out T value)
+    {
+        var uri = manager.ToAbsoluteUri(manager.Uri);
+
+        if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
+        {
+            if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
+            {
+                value = (T)(object)valueAsInt;
+                return true;
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                value = (T)(object)valueFromQueryString.ToString();
+                return true;
+            }
+
+            if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
+            {
+                value = (T)(object)valueAsDecimal;
+                return true;
+            }
+        }
+
+        value = default;
+        return false;
+    }
+
+    public static ListQuery GetListQuery(this NavigationManager manager)
+    {
+        manager.TryGetQueryString<int>("pageIndex", out int _pageIndex);
+        manager.TryGetQueryString<int>("pageSize", out int _pageSize);
+        var listQuery = ListQuery.DefaultListQuery;
+        if (_pageIndex > 0)
+        {
+            listQuery.PageIndex = _pageIndex;
+        }
+        if (_pageSize > 0)
+        {
+            listQuery.PageSize = _pageSize;
+        }
+        return listQuery;
+    }
 }
