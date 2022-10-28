52073cd80d0e4f7e0eaac399aabd3dbe0670ad68
Alex Oswald <alex.oswald@outlook.com>
2022-10-28 01:10:24 -0700

    Need to change how the code gen works

---
+++ src/AutoHome.RestClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using System.Diagnostics;
+
+var json = await File.ReadAllTextAsync("swagger.json");
+var document = await OpenApiDocument.FromJsonAsync(json);
+
+var settings = new CSharpClientGeneratorSettings
+{
+    ClassName = "MyClass",
+    CSharpGeneratorSettings =
+    {
+        Namespace = "MyNamespace"
+    }
+};
+
+var generator = new CSharpClientGenerator(document, settings);
+var code = generator.GenerateFile();
+
+File.WriteAllText("RestClient.cs", code);
+
+Debugger.Break();
