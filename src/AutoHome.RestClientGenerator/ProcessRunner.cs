645f28ee2c15153e1cc9163cad53982bc1b9be62
Alex Oswald <alex.oswald@outlook.com>
2022-10-29 13:50:50 -0700

    The client generator will now generate swagger.json and generate the rest client class

---
+++ src/AutoHome.RestClientGenerator/ProcessRunner.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+public class ProcessRunner
+{
+    private readonly string _workingDirectory;
+
+    public ProcessRunner(string workingDirectory)
+    {
+        _workingDirectory = workingDirectory;
+    }
+
+    public void Invoke(string program, string arguments = "")
+    {
+        ProcessStartInfo processStartInfo = new()
+        {
+            WorkingDirectory = _workingDirectory,
+            FileName = program,
+            Arguments = arguments
+        };
+        Process? process = Process.Start(processStartInfo);
+        if (process is null)
+        {
+            throw new NullReferenceException("Process is null, it was not created");
+        }
+
+        process.WaitForExit();
+    }
+}
--- src/AutoHome.RestClientGenerator/Program.cs
+++ src/AutoHome.RestClientGenerator/Program.cs
@@ -1,13 +1,23 @@
 ﻿using NJsonSchema.CodeGeneration.CSharp;
 using NSwag;
 using NSwag.CodeGeneration.CSharp;
-using System.Diagnostics;
 
-var json = await File.ReadAllTextAsync("swagger.json");
+// Program should be running in ROOT\src\AutoHome.RestClientGenerator\bin\Debug\net6.0\
+// Our working directory is in the src folder so lets go up 5 directories
+var root = "..\\..\\..\\..\\..\\";
+var runner = new ProcessRunner($"{root}src\\AutoHome.Server");
+
+// Restore dotnet swagger tool
+runner.Invoke("dotnet", "tool restore");
+// Invoke the swagger cli to generate swagger.json in the AutoHome.RestClient project
+runner.Invoke("dotnet", "swagger tofile --output ..\\AutoHome.RestClientGenerator\\swagger.json bin\\Debug\\net6.0\\AutoHome.Server.dll v1");
+
+// Open swagger.json as an api document
+var json = await File.ReadAllTextAsync($"{root}src\\AutoHome.RestClientGenerator\\swagger.json");
 var document = await OpenApiDocument.FromJsonAsync(json);
 
+// Setup code generation settings
 string className = "AutoHomeRestClient";
-
 var settings = new CSharpClientGeneratorSettings
 {
     ClassName = className,
@@ -18,11 +28,11 @@ var settings = new CSharpClientGeneratorSettings
     }
 };
 
+// Generate the rest client
 var generator = new CSharpClientGenerator(document, settings);
 var code = generator.GenerateFile();
 
-File.WriteAllText($"..\\..\\..\\..\\AutoHome.RestClient\\{className}.cs", code);
+// Write the rest client class to the AutoHome.RestClient porject
+File.WriteAllText($"{root}src\\AutoHome.RestClient\\{className}.cs", code);
 
 Console.WriteLine($"{className}.cs generated");
-
-Debugger.Break();
