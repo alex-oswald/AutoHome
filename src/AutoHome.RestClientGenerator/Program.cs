841301ba1ebc85d6983a54213ac75bc3ca7d6aa8
Alex Oswald <alex.oswald@outlook.com>
2022-10-29 12:04:14 -0700

    Rename project

--- src/AutoHome.RestClientGenerator/Program.cs
+++ src/AutoHome.RestClientGenerator/Program.cs
@@ -1,22 +1,28 @@
-﻿using NSwag;
+﻿using NJsonSchema.CodeGeneration.CSharp;
+using NSwag;
 using NSwag.CodeGeneration.CSharp;
 using System.Diagnostics;
 
 var json = await File.ReadAllTextAsync("swagger.json");
 var document = await OpenApiDocument.FromJsonAsync(json);
 
+string className = "AutoHomeRestClient";
+
 var settings = new CSharpClientGeneratorSettings
 {
-    ClassName = "MyClass",
+    ClassName = className,
     CSharpGeneratorSettings =
     {
-        Namespace = "MyNamespace"
+        Namespace = "AutoHome.Api",
+        JsonLibrary = CSharpJsonLibrary.SystemTextJson,
     }
 };
 
 var generator = new CSharpClientGenerator(document, settings);
 var code = generator.GenerateFile();
 
-File.WriteAllText("RestClient.cs", code);
+File.WriteAllText($"..\\..\\..\\..\\AutoHome.Client\\{className}.cs", code);
+
+Console.WriteLine($"{className}.cs generated");
 
 Debugger.Break();
