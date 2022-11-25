99a95a50f6aeaaf7918c9a2fdf7cd05f058e020d
Alex Oswald <alex.oswald@outlook.com>
2022-11-25 00:12:23 -0800

    Update

---
+++ src/AutoHome.Client/Components/Video/HLSStream.razor.js
@@ -0,0 +1,15 @@
+﻿var video = document.getElementById('video');
+var videoSrc = 'https://wyze.pi.lan:8888/deck/stream.m3u8';
+//
+// First check for native browser HLS support
+//
+if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = videoSrc;
+    //
+    // If no native HLS support, check if HLS.js is supported
+    //
+} else if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+}
