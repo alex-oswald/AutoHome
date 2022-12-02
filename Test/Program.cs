74e814d16def803455b3e7d5f9da395a1ba3cdfa
Alex Oswald <alex.oswald@outlook.com>
2022-12-01 19:05:47 -0800

    Update

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,11 @@
+using nanoFramework.M2Mqtt;
 using nanoFramework.Networking;
 using nanoFramework.WebServer;
 using System;
 using System.Device.Gpio;
+using System.Device.Wifi;
 using System.Diagnostics;
+using System.Text;
 using System.Threading;
 
 namespace Test
@@ -10,7 +13,8 @@ namespace Test
     public static class Configuration
     {
         public const string IntegrationDeviceId = "8d291f88-3b06-428c-87fb-ecd0eea44d17";
-        
+        public const string MqttBroker = "pi.lan";
+
         public static class Wifi
         {
             public const string SSID = "";
@@ -31,32 +35,46 @@ namespace Test
                 _controller.OpenPin(2, PinMode.Output);
                 _controller.Write(2, PinValue.Low);
 
-                Thread blink = new Thread(() => Blink(_controller, CancellationToken.None));
+                Thread blink = new Thread(() => BlinkSlow(_controller, CancellationToken.None));
                 blink.Start();
 
-
-
+            wifi_connect:
                 CancellationTokenSource cs = new(60000);
-                var success = WifiNetworkHelper.ConnectDhcp(Configuration.Wifi.SSID, Configuration.Wifi.Password, requiresDateTime: true, token: cs.Token);
+                WifiNetworkHelper.SetupNetworkHelper(true);
+                var success = WifiNetworkHelper.ConnectDhcp(Configuration.Wifi.SSID, Configuration.Wifi.Password, WifiReconnectionKind.Automatic, requiresDateTime: true, token: cs.Token);
                 if (!success)
                 {
-                    //_controller.Write(2, PinValue.Low);
                     Debug.WriteLine($"Can't get a proper IP address and DateTime, error: {WifiNetworkHelper.Status}.");
                     if (WifiNetworkHelper.HelperException != null)
                     {
                         Debug.WriteLine($"Exception: {WifiNetworkHelper.HelperException}");
                     }
-                    return;
+                    // Start over trying to connect to wifi
+                    Debug.WriteLine("Go back and try to connect to WiFi again");
+                    goto wifi_connect;
                 }
-
+                Debug.WriteLine("Connected to WiFi");
                 blink.Abort();
 
-
+                // Setup web server
                 _controller.Write(2, PinValue.Low);
                 using WebServer server = new(80, HttpProtocol.Http, new Type[] { typeof(Controller), typeof(ControllerHello) });
                 server.ApiKey = Configuration.IntegrationDeviceId;
                 server.Start();
+                Debug.WriteLine("Web server started");
 
+                // Setup MQTT
+                MqttClient client = new MqttClient("pi.lan", 1883, false, null, null, MqttSslProtocols.None);
+                client.ProtocolVersion = MqttProtocolVersion.Version_5;
+                client.Connect(Configuration.IntegrationDeviceId, true);
+                client.Publish("device/test/status", Encoding.UTF8.GetBytes("Device is online"));
+
+                while (true)
+                {
+                    Thread.Sleep(30000);
+                    BlinkHigh(_controller, CancellationToken.None);
+                    client.Publish("device/test/status", Encoding.UTF8.GetBytes("heartbeat"));
+                }
 
                 Thread.Sleep(Timeout.Infinite);
             }
@@ -70,23 +88,26 @@ namespace Test
             }
         }
 
-        public static void Blink(GpioController controller, CancellationToken cancellationToken)
+        public static void BlinkSlow(GpioController controller, CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
                 controller.Write(2, PinValue.High);
-                Thread.Sleep(50);
+                Thread.Sleep(250);
                 controller.Write(2, PinValue.Low);
-                Thread.Sleep(50);
-                controller.Write(2, PinValue.High);
-                Thread.Sleep(50);
-                controller.Write(2, PinValue.Low);
-                Thread.Sleep(50);
-                controller.Write(2, PinValue.High);
-                Thread.Sleep(50);
-                controller.Write(2, PinValue.Low);
-                Thread.Sleep(750);
+                Thread.Sleep(1750);
             }
+        }
+
+        public static void BlinkHigh(GpioController controller, CancellationToken cancellationToken)
+        {
+            controller.Write(2, PinValue.High);
+            Thread.Sleep(50);
+            controller.Write(2, PinValue.Low);
+            Thread.Sleep(50);
+            controller.Write(2, PinValue.High);
+            Thread.Sleep(50);
+            controller.Write(2, PinValue.Low);
         }
     }
 }
